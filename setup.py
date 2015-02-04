41842b0d0f51c9d1c57547a0041193eec3a700b9
Marijn Giesen <marijn@studio-donder.nl>
2015-02-04 14:44:07 +0100

    Work on package

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ setup(
     version='0.0.1',
     packages=['cronrunner'],
     package_dir={'cronrunner': 'cronrunner'},
-    package_data={'cronrunner': ['config/cron-runner.yaml.dist']},
+    package_data={'cronrunner': ['config/*']},
     url='https://github.com/marijngiesen/cron-runner',
     license='Apache 2.0',
     author='Marijn Giesen',
     author_email='marijn@studio-donder.nl',
     description='Runner of cron jobs with logging and duration timing',
-    include_package_data=True,
     entry_points={
         'console_scripts': [
             'cron-runner = cronrunner.main:main',
