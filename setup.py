4e4a3c700d2e871e5e60c73753512e08e8ad207f
Marijn Giesen <marijn@studio-donder.nl>
2015-02-04 14:23:01 +0100

    Also copy config

--- setup.py
+++ setup.py
@@ -4,15 +4,18 @@ setup(
     name='cron-runner',
     version='0.0.1',
     packages=['cronrunner'],
+    # package_data={'cronrunner': 'c'},
     url='https://github.com/marijngiesen/cron-runner',
     license='Apache 2.0',
     author='Marijn Giesen',
     author_email='marijn@studio-donder.nl',
     description='Runner of cron jobs with logging and duration timing',
+    include_package_data=True,
     entry_points={
         'console_scripts': [
             'cron-runner = cronrunner.main:main',
         ]
     },
-    requires=['requests', 'peewee', 'pyyaml', 'commandr']
+    requires=['requests', 'peewee', 'pyyaml', 'commandr'],
+    # data_files=[('/etc', ['etc/cron-runner.yaml.dist'])]
 )
